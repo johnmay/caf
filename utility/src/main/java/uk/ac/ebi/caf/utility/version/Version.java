ea617df88f6d6b78bc48d555e9c6512cd25fc0a8
John May <john.wilkinsonmay@gmail.com>
2012-04-05 17:09:45 +0100

    Added ability to create version number from a string

--- utility/src/main/java/uk/ac/ebi/caf/utility/version/Version.java
+++ utility/src/main/java/uk/ac/ebi/caf/utility/version/Version.java
@@ -18,25 +18,26 @@ package uk.ac.ebi.caf.utility.version;
  * see <http://www.gnu.org/licenses/>.
  * 
  */
+
 import org.apache.log4j.Logger;
+
+import java.util.regex.Pattern;
 
 
 /**
+ * Version 2012.01.31
  *
- *          Version 2012.01.31
+ * @author johnmay
+ * @author $Author$ (this version)
+ *         <p/>
+ *         Class describes a 4 digit version. The digits represent the major,
+ *         minor, sub and dev version iterations. The major version can take a
+ *         value 0-255 whilst the others 0-16. The 4 digits are condensed to a
+ *         single integer index {@see getIndex()} which can easily be compared
+ *         and ordered
  * @version $Rev$ : Last Changed $Date$
- * @author  johnmay
- * @author  $Author$ (this version)
- *
- *          Class describes a 4 digit version. The digits represent the major,
- *          minor, sub and dev version iterations. The major version can take a 
- *          value 0-255 whilst the others 0-16. The 4 digits are condensed to a 
- *          single integer index {@see getIndex()} which can easily be compared
- *          and ordered
- * 
- *
  */
-public class Version {
+public class Version implements Comparable<Version> {
 
     private static final Logger LOGGER = Logger.getLogger(Version.class);
 
@@ -44,13 +45,15 @@ public class Version {
 
     private String hex;
 
+    private Pattern split = Pattern.compile("[^\\d]+");
 
     /**
      * Create a new version specifying the 4 digits.
+     *
      * @param i
      * @param j
      * @param k
-     * @param l 
+     * @param l
      */
     public Version(int i,
                    int j,
@@ -58,20 +61,48 @@ public class Version {
                    int l) {
 
         hex =
-        String.format("%02x", i)
-        + Integer.toHexString(j & 0xF)
-        + Integer.toHexString(k & 0xF)
-        + Integer.toHexString(l & 0xF);
+                String.format("%02x", i)
+                        + Integer.toHexString(j & 0xF)
+                        + Integer.toHexString(k & 0xF)
+                        + Integer.toHexString(l & 0xF);
 
 
         index = Integer.parseInt(hex.substring(0, 5), 16);
 
     }
 
+    /**
+     * Create a version instance from a string. The string is split
+     * on non number characters and is used to fill up the version
+     * number. If too few number are provided then the version is
+     * padded with 0's. i.e  "0.9" -> "0.9.0.0". If too many digits
+     * are found then the value is truncated "0.9.0.0.1" -> "0.9.0.0"
+     *
+     * @param version the version id to create
+     */
+    public Version(String version) {
+
+        String[] values = split.split(version);
+        int[] base = new int[]{0, 0, 0, 0};
+        for (int i = 0; i < values.length; i++) {
+            base[i] = Integer.parseInt(values[i]);
+        }
+
+        hex =
+                String.format("%02x", base[0])
+                        + Integer.toHexString(base[1] & 0xF)
+                        + Integer.toHexString(base[2] & 0xF)
+                        + Integer.toHexString(base[3] & 0xF);
+
+
+        index = Integer.parseInt(hex.substring(0, 5), 16);
+
+    }
 
     /**
      * Create a new version from an index (useful for serialization)
-     * @param index 
+     *
+     * @param index
      */
     public Version(int index) {
         this.index = index;
@@ -81,7 +112,8 @@ public class Version {
 
     /**
      * Access the hexadecimal value
-     * @return 
+     *
+     * @return
      */
     public String getHex() {
         return hex;
@@ -90,7 +122,8 @@ public class Version {
 
     /**
      * Access the index
-     * @return 
+     *
+     * @return
      */
     public int getIndex() {
         return index;
@@ -99,7 +132,8 @@ public class Version {
 
     /**
      * Access the major version (first digit)
-     * @return 
+     *
+     * @return
      */
     public int getMajorVersion() {
         return Integer.parseInt(hex.substring(0, 2), 16);
@@ -108,7 +142,8 @@ public class Version {
 
     /**
      * Access the minor version (second digit)
-     * @return 
+     *
+     * @return
      */
     public int getMinorVersion() {
         return Integer.parseInt(hex.substring(2, 3), 16);
@@ -117,7 +152,8 @@ public class Version {
 
     /**
      * Access the sub version (third digit)
-     * @return 
+     *
+     * @return
      */
     public int getSubVesion() {
         return Integer.parseInt(hex.substring(3, 4), 16);
@@ -126,7 +162,8 @@ public class Version {
 
     /**
      * Access the dev version (forth digit)
-     * @return 
+     *
+     * @return
      */
     public int getDevVersion() {
         return Integer.parseInt(hex.substring(4, 5), 16);
@@ -138,13 +175,17 @@ public class Version {
 
         StringBuilder sb = new StringBuilder();
 
-        sb.append("v");
         sb.append(getMajorVersion()).append(".");
-        sb.append(getMinorVersion()).append("/");
-        sb.append(getSubVesion()).append("-");
+        sb.append(getMinorVersion()).append(".");
+        sb.append(getSubVesion()).append(".");
         sb.append(getDevVersion());
 
         return sb.toString();
 
+    }
+
+    @Override
+    public int compareTo(Version o) {
+        return ((Integer) o.getIndex()).compareTo(getIndex());
     }
 }
