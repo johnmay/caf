57c8f7f9d0380a06c025ed11328bf2d697bbe080
John May <john.wilkinsonmay@gmail.com>
2012-06-20 09:02:38 +0100

    Added OS App Data resolution for file preference

--- utility/src/main/java/uk/ac/ebi/caf/utility/preference/type/FilePreference.java
+++ utility/src/main/java/uk/ac/ebi/caf/utility/preference/type/FilePreference.java
@@ -18,26 +18,30 @@
  */
 package uk.ac.ebi.caf.utility.preference.type;
 
-import java.io.File;
-import java.util.prefs.Preferences;
 import org.apache.log4j.Logger;
 import uk.ac.ebi.caf.utility.preference.AbstractPreference;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.prefs.Preferences;
+
 
 /**
+ * FilePreference 2012.01.29
  *
- *          FilePreference 2012.01.29
+ * @author johnmay
+ * @author $Author$ (this version)
+ *         <p/>
+ *         Class description
  * @version $Rev$ : Last Changed $Date$
- * @author  johnmay
- * @author  $Author$ (this version)
- *
- *          Class description
- *
  */
 public class FilePreference extends AbstractPreference<File> {
 
     private static final Logger LOGGER = Logger.getLogger(FilePreference.class);
 
+    public static final File   OS_APP_DATA_FILE = getOSAppDataRoot();
+    public static final String OS_APP_DATA_PATH = OS_APP_DATA_FILE.getAbsolutePath().replaceAll("\\\\", "\\\\\\\\");
 
     public FilePreference(File value,
                           Preferences scope,
@@ -49,13 +53,98 @@ public class FilePreference extends AbstractPreference<File> {
     }
 
 
+    /**
+     * Access the location of the file. Special use:
+     * <p/>
+     * <pre>{@code
+     *  <os.app.data> is replaced with the OS Dependant application data folder
+     *  <user.home> is replaced with the users home directory
+     * <p/>
+     * }</pre>
+     *
+     * @return
+     */
     public File get() {
+
         String path = getScope().get(getKey(), "");
-        return path.isEmpty() ? getDefault() : new File(path);
+
+        String value = path.isEmpty() ? getDefault().getPath() : path;
+
+        value = value.replaceAll("<os.app.data>", OS_APP_DATA_PATH);
+        value = value.replaceAll("<user.home>", System.getProperty("user.home"));
+
+        return new File(value);
+
     }
 
 
     public void put(File value) {
         getScope().put(getKey(), value.getPath());
     }
+
+    /**
+     * Access the AppData root dependant or os
+     * <p/>
+     * Win -> %AppData%
+     * OSX -> ~/Library/Application Support/
+     * Unix -> ~/.appdata
+     * Other -> user.dir
+     *
+     * @return
+     */
+    private static File getOSAppDataRoot() {
+
+        String OS = System.getProperty("os.name").toUpperCase();
+
+        if (OS.contains("WIN")) {
+            // make it easier to handle from within java
+            return new File(System.getenv("APPDATA"));
+        } else if (OS.contains("MAC")) {
+            return new File(System.getProperty("user.home")
+                                    + File.separator + "Library"
+                                    + File.separator + "Application\\ Support");
+        } else if (OS.contains("NUX")) {
+
+            // would be good to write an program specific name
+            File appdata = new File(System.getProperty("user.home")
+                                            + File.separator + ".appdata");
+
+            File readme = new File(appdata, "README");
+
+            // create an place a README
+            if (!readme.exists()) {
+                if (appdata.mkdirs()) {
+                    FileWriter writer = null;
+
+                    try {
+                        writer = new FileWriter(readme);
+                        writer.write(".appdata is the default folder used by the creative application" +
+                                             " framework http://www.github.com/johnmay/creative-application-framework");
+                        writer.write("The creative application framework is currently used by MDK and Metingear but" +
+                                             "may be useed by other applications in future.");
+                    } catch (IOException e) {
+                        LOGGER.error("Could not create README file for ~/.appdata");
+                    } finally {
+                        try {
+                            if (writer != null) {
+                                writer.close();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                        }
+                    }
+
+
+                }
+            }
+
+            return appdata;
+
+        }
+
+        return new File(System.getProperty("user.dir"));
+
+    }
+
+
 }
