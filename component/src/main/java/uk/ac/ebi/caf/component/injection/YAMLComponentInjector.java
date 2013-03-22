38f27027cf95f5a36e4d7d90b4531045ea462cb3
John May <john.wilkinsonmay@gmail.com>
2013-03-22 08:56:41 +0000

    handling for multiple resources

--- component/src/main/java/uk/ac/ebi/caf/component/injection/YAMLComponentInjector.java
+++ component/src/main/java/uk/ac/ebi/caf/component/injection/YAMLComponentInjector.java
@@ -10,6 +10,8 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.security.InvalidParameterException;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 
 import static uk.ac.ebi.caf.utility.ResourceUtility.getIcon;
@@ -24,9 +26,31 @@ public class YAMLComponentInjector
         extends AbstractComponentInjector
         implements ComponentInjector {
 
-    private static final Logger LOGGER = Logger.getLogger(YAMLComponentInjector.class);
+    private static final Logger LOGGER = Logger
+            .getLogger(YAMLComponentInjector.class);
 
-    private Map<String, Map<String, Map<String, String>>> map;
+    private Map<String, Map<String, Map<String, String>>> map = new HashMap<String, Map<String, Map<String, String>>>(50);
+
+    public YAMLComponentInjector(String path) throws IOException {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+
+        Enumeration<URL> urls = loader.getResources(path);
+        while (urls.hasMoreElements()) {
+            URL url = urls.nextElement();
+            InputStream in = url.openStream();
+            if (in != null) {
+                try {
+                    // note - may clobber if duplicate top level but as the top
+                    // level is a class name this should be fine
+                    map.putAll(load(in));
+                } catch (IOException e) {
+                    throw e;
+                } finally {
+                    in.close();
+                }
+            }
+        }
+    }
 
     public YAMLComponentInjector(URL url) throws IOException {
         this(url.openStream());
@@ -44,12 +68,21 @@ public class YAMLComponentInjector
         Object data = yaml.load(reader);
 
         if (isValid(data)) {
-            map = (Map<String, Map<String, Map<String, String>>>) data;
+            map.putAll((Map<String, Map<String, Map<String, String>>>) data);
         } else {
             throw new InvalidParameterException("Map<String,Map<String,String>> expected in yaml file");
         }
+    }
 
-
+    private Map<String, Map<String, Map<String, String>>> load(InputStream in) throws
+                                                                               IOException {
+        Yaml yaml = new Yaml();
+        Object data = yaml.load(in);
+        if (isValid(data)) {
+            return (Map<String, Map<String, Map<String, String>>>) data;
+        } else {
+            throw new IOException("Map<String,Map<String,String>> expected in yaml file");
+        }
     }
 
     private boolean isValid(Object data) {
@@ -89,7 +122,8 @@ public class YAMLComponentInjector
     public void inject(Class c, JComponent component, String fieldName) {
 
         if (map.containsKey(c.getSimpleName())) {
-            Map<String, Map<String, String>> valueMap = map.get(c.getSimpleName());
+            Map<String, Map<String, String>> valueMap = map
+                    .get(c.getSimpleName());
             if (valueMap.containsKey(fieldName)) {
 
                 Map<String, String> properties = valueMap.get(fieldName);
@@ -106,12 +140,16 @@ public class YAMLComponentInjector
                     setIcon(component, getIcon(c, properties.get("icon")));
                 }
 
-                if (properties.containsKey("selectedIcon") && canSetSelectedIcon(component)) {
-                    setSelectedIcon(component, getIcon(c, properties.get("icon")));
+                if (properties
+                        .containsKey("selectedIcon") && canSetSelectedIcon(component)) {
+                    setSelectedIcon(component, getIcon(c, properties
+                            .get("icon")));
                 }
 
-                if (properties.containsKey("disabledIcon") && canSetIcon(component)) {
-                    setDisabledIcon(component, getIcon(c, properties.get("icon")));
+                if (properties
+                        .containsKey("disabledIcon") && canSetIcon(component)) {
+                    setDisabledIcon(component, getIcon(c, properties
+                            .get("icon")));
                 }
 
 
